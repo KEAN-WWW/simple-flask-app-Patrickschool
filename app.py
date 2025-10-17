26f9993c6898a2f65d9e6f225022c56be8fcdea4
shyeim Powell <shyeimnorman@gmail.com>
2025-10-17 15:02:17 -0400

    Initial Flask app commit

--- app.py
+++ app.py
@@ -5,25 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Default route that prints Hello CPS3500!"""
     return 'Hello CPS3500!'
 
 
 @app.route('/new_page')
 def new_page():
-    """New page route that prints This is a New Page!"""
     return 'This is a New Page!'
 
 
 @app.route('/user/<username>')
 def user(username):
-    """
-    Render a template and pass the username variable.
-    The template will detect if the username is 'jack'.
-    """
     return render_template('user.html', username=username)
 
 
-# Only include this block if you want to run app/app.py directly.
 if __name__ == '__main__':
     app.run(debug=True)
